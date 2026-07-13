d989552fb13c230904cf8b526352422829dda055
jiminkk <jasmine.kim@medallion.fm>
2026-07-13 15:07:59 -0400

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Counter } from "./Counter";
+import { Counter } from "./Counter-tmp";
 
 const App = () => {
   return (
     <div>
       <Counter initCount={1} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
